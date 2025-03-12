ed57388688a16098f0c738354ae17486ebcae38d
glucoseck007 <khicongkhanh@gmail.com>
2025-03-12 19:46:21 +0700

    update server

---
+++ candle-tales-server/src/main/java/org/example/candletalesserver/CandleTalesServerApplication.java
@@ -0,0 +1,13 @@
+package org.example.candletalesserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CandleTalesServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CandleTalesServerApplication.class, args);
+    }
+
+}
---
+++ candle-tales-server/src/test/java/org/example/candletalesserver/CandleTalesServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.candletalesserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CandleTalesServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
